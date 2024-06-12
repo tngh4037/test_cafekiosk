7aae1a08349f30d73b91ac31924c9a76e01ff177
tngh4037 <tngh4037@gmail.com>
2024-06-13 05:54:31 +0900

    section5: Spring & JPA 기반 테스트 - Persistence Layer 테스트(1)

---
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
@@ -0,0 +1,15 @@
+package sample.cafekiosk.spring.domain.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    /**
+     * select *
+     * from product
+     * where selling_status in ('SELLING', 'HOLD');
+     */
+    List<Product> findAllBySellingStatusIn(List<ProductSellingStatus> sellingStatuses); // 쿼리 메서드 기능
+}
