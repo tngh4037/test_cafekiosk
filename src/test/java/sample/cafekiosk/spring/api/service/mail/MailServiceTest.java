5824e18cef2171535c826b444c2d6fe62465981d
tngh4037 <tngh4037@gmail.com>
2024-06-19 01:40:23 +0900

    section6: Mock을 마주하는 자세 - BDDMockito

--- src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -21,8 +21,8 @@ import static org.mockito.Mockito.times;
 @ExtendWith(MockitoExtension.class) // @Mock 애노테이션을 사용해서 Mock 객체를 생성하기 위해서는 @ExtendWith(MockitoExtension.class) 를 적용해줘야 한다. ( 그래야 테스트가 시작될 때, 우리는 Mockito 를 사용해서 Mock 을 만들거야라는 것을 인지할 수 있다. )
 class MailServiceTest {
 
-    //@Mock
-    @Spy // 일부만 stubbing 하고, 나머지는 실제 객체가 그대로 동작하도록 하고자 할 때
+    @Mock
+    // @Spy // 일부만 stubbing 하고, 나머지는 실제 객체가 그대로 동작하도록 하고자 할 때
     private MailSendClient mailSendClient;
 
     @Mock
@@ -37,9 +37,7 @@ class MailServiceTest {
         // given
         MailSendClient mailSendClient = Mockito.mock(MailSendClient.class);
         MailSendHistoryRepository mailSendHistoryRepository = Mockito.mock(MailSendHistoryRepository.class);
-
         MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
-
         // stubbing
         Mockito.when(mailSendClient.sendEmail(
                 ArgumentMatchers.anyString(), ArgumentMatchers.anyString(),
@@ -58,12 +56,34 @@ class MailServiceTest {
     @Test
     public void sendMail2() throws Exception {
 
-        // stubbing
-        doReturn(true)
+        // given
+        Mockito.doReturn(true)
                 .when(mailSendClient)
                 .sendEmail(
                         ArgumentMatchers.anyString(), ArgumentMatchers.anyString(),
                         ArgumentMatchers.anyString(), ArgumentMatchers.anyString());
+
+        // when
+        boolean result = mailService.sendMail("", "", "", "");
+
+        // then
+        assertThat(result).isTrue();
+        Mockito.verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
+    }
+
+    @DisplayName("메일 전송 테스트3 - BDDMockito")
+    @Test
+    public void sendMail3() throws Exception {
+
+        // given
+        // Mockito.when(mailSendClient.sendEmail(
+        //                 ArgumentMatchers.anyString(), ArgumentMatchers.anyString(),
+        //                 ArgumentMatchers.anyString(), ArgumentMatchers.anyString()))
+        //         .thenReturn(true);
+        BDDMockito.given(mailSendClient.sendEmail(
+                        ArgumentMatchers.anyString(), ArgumentMatchers.anyString(),
+                        ArgumentMatchers.anyString(), ArgumentMatchers.anyString()))
+                .willReturn(true);
 
         // when
         boolean result = mailService.sendMail("", "", "", "");
